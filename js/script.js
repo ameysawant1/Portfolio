d387a9b2aa1099c40c77eee72cf95bfb68fb0dac
Amey Sawant <amey123sawant@gmail.com>
2024-08-16 19:29:17 +0530

    Hero Section, background gradient animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     const loadingScreen = document.querySelector('.loading-screen');
     const loadingWords = document.querySelector('.loading-words');
     const homePage = document.querySelector('main');
@@ -74,3 +75,61 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+function magneticMenuItems() {
+    // Add classes to menu items and their links
+    document.querySelectorAll(".menu-item").forEach(item => item.classList.add("magnetic"));
+    document.querySelectorAll(".menu-item a").forEach(link => link.classList.add("link"));
+
+    // Select all elements with the 'magnetic' class
+    var elements = document.querySelectorAll(".magnetic");
+    var t = 25; // Range of the magnetic effect
+
+    // Apply the magnetic effect only if the window width is greater than 540 pixels
+    if (window.innerWidth > 540) {
+        elements.forEach(element => {
+            // Add mousemove and mouseout event listeners to each magnetic element
+            element.addEventListener("mousemove", handleMouseMove);
+            element.addEventListener("mouseout", function(e) {
+                // Return the element to its original position on mouseout
+                gsap.to(e.currentTarget, {
+                    duration: 1,
+                    x: 0,
+                    y: 0,
+                    ease: "power4.out"
+                });
+            });
+        });
+
+        function handleMouseMove(e) {
+            var element = e.currentTarget;
+            var rect = element.getBoundingClientRect();
+            // Calculate new position based on mouse position and animate the element
+            gsap.to(element, {
+                duration: 1,
+                x: ((e.clientX - rect.left) / element.offsetWidth - 0.6) * t,
+                y: ((e.clientY - rect.top) / element.offsetHeight - 0.6) * t,
+                ease: "power4.out"
+            });
+        }
+    }
+}
+
+// Call the function after the DOM is ready
+document.addEventListener("DOMContentLoaded", function() {
+    magneticMenuItems();
+});
+
+grained('#grained', {
+    animate: true,
+    patternWidth: 500, // Adjust as needed
+    patternHeight: 500, // Adjust as needed
+    grainOpacity: 0.06, // Adjust opacity as needed
+    grainDensity: 2.88, // Adjust density as needed
+    grainHeight: 2.5,   // Adjust height as needed
+    grainWidth: 2.5     // Adjust width as needed
+});
+
+
+
