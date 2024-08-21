11fb0f490112ee670ab80c76f81f70c4e5b3587b
Amey Sawant <amey123sawant@gmail.com>
2024-08-22 01:01:20 +0530

    added font

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
             currentWord++;
         } else {
             clearInterval(wordInterval);
-            setTimeout(pageTransitionIn, 100); // Ensure pageTransitionIn is called
+            setTimeout(pageTransitionIn, 100);
         }
     }
 
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
         loadingWords.style.opacity = '1';
         setTimeout(() => {
             loadingWords.style.opacity = '0';
-            loadingContainer.style.top = '-200%';
+            loadingContainer.style.top = '-100%';
             setTimeout(() => {
                 homePage.style.opacity = '1';
                 homePage.style.transform = 'translateY(0)';
@@ -40,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function pageTransitionIn() {
         loadingContainer.style.pointerEvents = 'none';
+        loadingContainer.style.display = 'none';
         loadingWords.style.opacity = '0';
         roundedDivTop.style.height = '0';
         roundedDivBottom.style.height = '0';
@@ -62,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
     initLoader();
 
     window.addEventListener('load', () => {
-        setTimeout(pageTransitionOut, 100); // Start the transition out after a slight delay
+        setTimeout(pageTransitionOut, 100);
     });
 
     navLinks.forEach(link => {
@@ -91,7 +92,7 @@ function magneticMenuItems() {
         elements.forEach(element => {
             // Add mousemove and mouseout event listeners to each magnetic element
             element.addEventListener("mousemove", handleMouseMove);
-            element.addEventListener("mouseout", function(e) {
+            element.addEventListener("mouseout", function (e) {
                 // Return the element to its original position on mouseout
                 gsap.to(e.currentTarget, {
                     duration: 1,
@@ -105,7 +106,7 @@ function magneticMenuItems() {
         function handleMouseMove(e) {
             var element = e.currentTarget;
             var rect = element.getBoundingClientRect();
-            // Calculate new position based on mouse position and animate the element
+
             gsap.to(element, {
                 duration: 1,
                 x: ((e.clientX - rect.left) / element.offsetWidth - 0.6) * t,
@@ -116,20 +117,53 @@ function magneticMenuItems() {
     }
 }
 
+function magneticLogo() {
+    // Select the logo element
+    var logo = document.querySelector(".logo-div");
+    var t = 30; // Range of the magnetic effect
+
+    // Apply the magnetic effect only if the window width is greater than 540 pixels
+    if (window.innerWidth > 540) {
+        // Add mousemove and mouseout event listeners to the logo
+        logo.addEventListener("mousemove", handleMouseMove);
+        logo.addEventListener("mouseout", function (e) {
+            // Return the logo to its original position on mouseout
+            gsap.to(e.currentTarget, {
+                duration: 1,
+                x: 0,
+                y: 0,
+                ease: "power4.out"
+            });
+        });
+
+        function handleMouseMove(e) {
+            var rect = logo.getBoundingClientRect();
+
+            gsap.to(logo, {
+                duration: 1,
+                x: ((e.clientX - rect.left) / logo.offsetWidth - 0.6) * t,
+                y: ((e.clientY - rect.top) / logo.offsetHeight - 0.6) * t,
+                ease: "power4.out"
+            });
+        }
+    }
+}
+
+// Initialize the magnetic effect for the logo when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", magneticLogo);
+
+
 // Call the function after the DOM is ready
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     magneticMenuItems();
 });
 
 grained('#grained', {
     animate: true,
-    patternWidth: 500, // Adjust as needed
-    patternHeight: 500, // Adjust as needed
-    grainOpacity: 0.06, // Adjust opacity as needed
-    grainDensity: 2.88, // Adjust density as needed
-    grainHeight: 2.5,   // Adjust height as needed
-    grainWidth: 2.5     // Adjust width as needed
+    patternWidth: 200,
+    patternHeight: 200,
+    grainOpacity: 0.05,
+    grainDensity: 2.80,
+    grainHeight: 2.1,
+    grainWidth: 2.1
 });
-
-
-
